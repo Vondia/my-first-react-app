58c409f7f8823e638fbb9cea9c1988db51045370
Vondia <p.dronk@hotmail.com>
2021-02-22 18:46:45 +0100

    don't want to lose progresse

--- src/components/pokemon.js
+++ src/components/pokemon.js
@@ -8,7 +8,7 @@ export default function Pokemon(props) {
       <p>Weight: {props.weight} kg</p>
       <p>Awesome: {props.awesome ? "YES!" : "nope, not really"}</p>
       <p>Terrifying: {props.terrifying ? "Very" : "nah, lovable"}</p>
-      <p>Abilities:</p>
+      <p>Abilities: ({props.abilities.length})</p>
       <ul>
         {props.abilities.map((ability) => {
           return <li>{ability}</li>;
