de3f7b7937f5bcd75daa705e5f3553bb8998f3cd
Vondia <p.dronk@hotmail.com>
2021-02-22 16:43:55 +0100

    lots of hard work on the 4 pokemon

--- src/App.js
+++ src/App.js
@@ -1,28 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Title from "./components/Title";
+import Pokemon from "./components/pokemon";
 
 function App() {
+  const pokemons = [
+    {
+      name: "Charizard",
+      weight: 90,
+      awesome: true,
+      terrifying: true,
+      abilities: ["Blaze", "Solar power", "Tough claws", "Drought"],
+    },
+    {
+      name: "Bulbasaur",
+      weight: 6.9,
+      awesome: true,
+      terrifying: true,
+      abilities: ["Overgrow", "Chlorophyll"],
+    },
+    {
+      name: "Mewtwo",
+      weight: 122,
+      awesome: true,
+      terrifying: false,
+      abilities: ["Pressure", "Unnerve", "Steadfast", "Insomnia"],
+    },
+    {
+      name: "Mega beedrill",
+      weight: 65,
+      awesome: false,
+      terrifying: false,
+      abilities: ["Intimidate", "Unnerve"],
+    },
+  ];
   return (
     <div className="App">
+      <main>
+        <Title content="Some Simple Title" />
+        {pokemons.map((pokemon, index) => (
+          <Pokemon
+            key={index}
+            name={pokemon.name}
+            weight={pokemon.weight}
+            awesome={pokemon.awesome}
+            terrifying={pokemon.terrifying}
+            abilities={pokemon.abilities}
+          />
+        ))}
+      </main>
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <p2>What does this look like?</p2>
-        <ul>And what if i add a list:
-          <li>What</li>
-          <li>an</li>
-          <li>awesome</li>
-          <li>test.</li>
-        </ul>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Welcome to codaisseur!
+          Learn React
         </a>
       </header>
     </div>
