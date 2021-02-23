b9aab3482d48277b9f7eaa8d5dd740fbf62b83ed
Vondia <p.dronk@hotmail.com>
2021-02-23 11:26:50 +0100

    Final commit before moving on to day2

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import logo from "./logo.svg";
 import "./App.css";
 
 import Title from "./components/Title";
-import Pokemon from "./components/pokemon";
+import Pokemon from "./components/Pokemon/pokemon";
 import Charizard from "./components/charizard";
 
 function App() {
-  const pokemons = [
+  const all_pokemon = [
     {
       name: "Charizard",
       weight: 90,
@@ -38,37 +38,54 @@ function App() {
     },
   ];
   return (
-    <div className="App">
-      <main>
-        <Title content="Some Simple Title" />
-        {pokemons.map((pokemon, index) => (
-          <Pokemon
-            key={index}
-            name={pokemon.name}
-            weight={pokemon.weight}
-            awesome={pokemon.awesome}
-            terrifying={pokemon.terrifying}
-            abilities={pokemon.abilities}
-          />
+    <main className="container">
+      <Title content="Some Simple Title" />
+      <div className="row">
+        {all_pokemon.map((pokemon) => (
+          <div className="col-md-6 col-lg-4">
+            <Pokemon
+              name={pokemon.name}
+              weight={pokemon.weight}
+              awesome={pokemon.awesome}
+              terrifying={pokemon.terrifying}
+              abilities={pokemon.abilities}
+            />
+          </div>
         ))}
-      </main>
-
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      </div>
+    </main>
   );
 }
 
 export default App;
+
+// return (
+//   <div className="App">
+//     <main>
+//       <Title content="Some Simple Title" />
+//       {pokemons.map((pokemon, index) => (
+//         <Pokemon
+//           key={index}
+//           name={pokemon.name}
+//           weight={pokemon.weight}
+//           awesome={pokemon.awesome}
+//           terrifying={pokemon.terrifying}
+//           abilities={pokemon.abilities}
+//         />
+//       ))}
+//     </main>
+
+//   <header className="App-header">
+//   <img src={logo} className="App-logo" alt="logo" />
+//   <p>
+//     Edit <code>src/App.js</code> and save to reload.
+//   </p>
+//   <a
+//     className="App-link"
+//     href="https://reactjs.org"
+//     target="_blank"
+//     rel="noopener noreferrer"
+//   >
+//     Learn React
+//   </a>
+// </header>;
